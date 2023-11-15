c34942282624959738f3ac74e60ee6422adb894e
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2023-11-15 19:14:14 -0300

    project created

---
+++ src/main/java/com/dslistgame/DslistgameApplication.java
@@ -0,0 +1,13 @@
+package com.dslistgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dslistgame/DslistgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dslistgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
