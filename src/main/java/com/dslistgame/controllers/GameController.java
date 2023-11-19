dd2e6435ece46188a27bce466c843242a2f6711d
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2023-11-19 10:33:12 -0300

    GameDTO and findById

--- src/main/java/com/dslistgame/controllers/GameController.java
+++ src/main/java/com/dslistgame/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dslistgame.dto.GameDTO;
 import com.dslistgame.dto.GameMinDTO;
 import com.dslistgame.services.GameService;
 
@@ -21,6 +23,12 @@ public class GameController {
 	@GetMapping
 	ResponseEntity<List<GameMinDTO>> findAll(){
 		List<GameMinDTO> dto = service.findAll();
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<GameDTO> findById(@PathVariable Long id){
+		GameDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 
--- src/main/java/com/dslistgame/services/GameService.java
+++ src/main/java/com/dslistgame/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dslistgame.dto.GameDTO;
 import com.dslistgame.dto.GameMinDTO;
 import com.dslistgame.entities.Game;
 import com.dslistgame.repositories.GameRepository;
@@ -15,9 +17,16 @@ public class GameService {
 	@Autowired
 	private GameRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> list = repository.findAll();
 		List<GameMinDTO> dto = list.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game entity = repository.findById(id).get();
+		GameDTO dto = new GameDTO(entity);
 		return dto;
 	}
 
